adbea9533dc08a71b0e26357acfe7f083ca2e828
Krishna3956 <130306857+Krishna3956@users.noreply.github.com>
2025-10-19 03:49:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isProduction = mode === "production";
 
   return {
-    root: process.cwd(),
     server: {
       host: "::",
       port: 8080,
     },
-    plugins: [react(), !isProduction && componentTagger()].filter(Boolean),
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
